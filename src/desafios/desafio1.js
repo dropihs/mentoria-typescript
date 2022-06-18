46b07bea1535bae930d8299555ba33affd274066
Pedro Soares <36679371+dropihs@users.noreply.github.com>
2022-06-18 15:20:50 -0300

    Update desafio1

--- src/desafios/desafio1.js
+++ src/desafios/desafio1.js
@@ -1,5 +1,26 @@
 // Como podemos rodar isso em um arquivo .ts sem causar erros? 
-
+/*
 let employee = {};
 employee.code = 10;
 employee.name = "John";
+*/
+//////////////////////////////////////////////////////////////////////
+
+let employee = {
+    code: 10,
+    name: "John"
+};
+
+
+
+//Criando interface
+/*
+interface Employee {
+    code: number,
+    name: string
+}
+
+let empObj = {} as Employee;
+empObj.code = 10;
+empObj.name = "John";
+*/
